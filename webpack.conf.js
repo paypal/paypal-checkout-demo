909a73dd60962d7d66b8c7f1cd89cb04e362987e
Andrew Throener <athroener@mystartupsolutions.com>
2017-04-17 15:52:21 -0500

    Initial Conversion

---
+++ webpack.conf.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/app/client/js/index.jsx'),
+        module: {
+            rules: [
+                {
+                    test: /sinon\.js$/,
+                    loader: "imports?define=>false,require=>false"
+                },
+                {
+                    test: /\.jsx?$/,
+                    exclude: /(sinon|chai)/,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.(html?|css|json|svg)$/,
+                    loader: 'raw-loader'
+                }
+            ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'src/app/build'),
+        filename: 'demo.js'
+    },
+    resolve: {
+        extensions: [ '.js', '.jsx' ]
+    }
+};

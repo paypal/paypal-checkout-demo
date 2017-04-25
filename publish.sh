56452b7fc91a5e8002300c8cc72ffbdf874dd50b
Daniel Brain <dbrain@paypal.com>
2017-04-25 11:15:36 -0700

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e;
+
+rm src/app/build/*;
+git checkout src/app/build/;
+
+if ! git diff-files --quiet; then
+    echo "Can not publish with unstaged uncommited changes";
+    exit 1;
+fi;
+
+if ! git diff-index --quiet --cached HEAD; then
+    echo "Can not publish with staged uncommited changes";
+    exit 1;
+fi;
+
+npm build;
+
+git add src/app/build --all;
+
+git commit -m "Dist" || echo "Nothing to distribute";
+
+npm version ${1-patch};
+
+git push;
+git push --tags;
+npm publish;

e0e10c4dfefd7eb4dfad1897a4e7c08318968b64
vishakha94 <vish.jeea@gmail.com>
2017-08-23 16:01:24 -0700

    Responsive button demo (#11)
    * Responsive button demo
    
    * Tagline silent demo

--- src/app/client/js/patterns/responsive.jsx
+++ src/app/client/js/patterns/responsive.jsx
@@ -1,51 +1,69 @@
 
 import React from 'react';
 
-export let generic = {
+export let responsive = {
 
-    slug: 'generic',
+    slug: 'responsive',
 
-    name: `Generic Button`,
+    name: `Responsive Button`,
 
-    fullName: `Express Generic Custom Button`,
+    fullName: `Express Checkout Custom Button with size as Responsive`,
 
     intro: (
-        <p>Customize your <b>PayPal Generic</b> button with colors, sizes and shapes.</p>
+        <p>If you set size: 'responsive', the button will match the width of the parent element, and auto-calculate an appropriate height based on the width.</p>
     ),
 
     code: (ctx) => `
         <script src="https://www.paypalobjects.com/api/checkout.js"></script>
 
+        <style>
+            
+            /* Media query for mobile viewport */
+            @media screen and (max-width: 400px) {
+                #paypal-button-container {
+                    width: 100%;
+                }
+            }
+            
+            /* Media query for desktop viewport */
+            @media screen and (min-width: 400px) {
+                #paypal-button-container {
+                    width: 250px;
+                    display: inline-block;
+                }
+            }
+            
+        </style>
+        
         <div id="paypal-button-container"></div>
-
+        
         <script>
-
-            // Render the PayPal button
-
+        
             paypal.Button.render({
-
+                
                 // Set your environment
-
+        
                 env: '${ctx.env}', // sandbox | production
-
+        
                 // Specify the style of the button
-
+        
                 style: {
-                    label: 'generic',
-                    size:  'medium',    // small | medium | large | responsive
-                    shape: 'rect',     // pill | rect
-                    color: 'black',     // gold | blue | silver | black
-                    tagline: false    
+                    label: 'checkout',  // checkout | credit | pay | buynow | generic
+                    size:  'responsive', // small | medium | large | responsive
+                    shape: 'pill',   // pill | rect
+                    color: 'gold'   // gold | blue | silver | black
                 },
-
+        
                 // PayPal Client IDs - replace with your own
                 // Create a PayPal app: https://developer.paypal.com/developer/applications/create
-
+        
                 client: {
                     sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
                     production: '<insert production client id>'
                 },
-
+        
+                // Wait for the PayPal button to be clicked
+        
                 payment: function(data, actions) {
                     return actions.payment.create({
                         payment: {
@@ -57,15 +75,18 @@ export let generic = {
                         }
                     });
                 },
-
+        
+                // Wait for the payment to be authorized by the customer
+        
                 onAuthorize: function(data, actions) {
                     return actions.payment.execute().then(function() {
                         window.alert('Payment Complete!');
                     });
                 }
-
+            
             }, '#paypal-button-container');
-
+        
         </script>
+
     `
 };

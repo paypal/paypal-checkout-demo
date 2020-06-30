f0aa670a4c5159d70bbc585ffa820a0b23d861c5
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2020-06-30 09:56:42 -0500

    Update error handling logic for INSTRUMENT_DECLINED and non-recoverable errors

--- src/app/client/js/patterns/server.jsx
+++ src/app/client/js/patterns/server.jsx
@@ -33,33 +33,48 @@ export let server = {
                 // Render the PayPal button into #paypal-button-container
                 paypal.Buttons({
 
-                    // Set up the transaction
+                    // Call your server to set up the transaction
                     createOrder: function(data, actions) {
                         return fetch('/demo/checkout/api/paypal/order/create/', {
                             method: 'post'
                         }).then(function(res) {
                             return res.json();
-                        }).then(function(data) {
-                            return data.id;
+                        }).then(function(orderData) {
+                            return orderData.id;
                         });
                     },
 
-                    // Finalize the transaction
+                    // Call your server to finalize the transaction
                     onApprove: function(data, actions) {
                         return fetch('/demo/checkout/api/paypal/order/' + data.orderID + '/capture/', {
                             method: 'post'
                         }).then(function(res) {
                             return res.json();
-                        }).then(function(details) {
-                            // Handle "INSTRUMENT_DECLINED" errors with a restart
-                            // when calling the Orders API from your server
-                            // https://developer.paypal.com/docs/checkout/integration-features/funding-failure/
-                            if (details.error === 'INSTRUMENT_DECLINED') {
+                        }).then(function(orderData) {
+                            // Three cases to handle:
+                            //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                            //   (2) Other non-recoverable errors -> Show a failure message
+                            //   (3) Successful transaction -> Show a success / thank you message
+
+                            // Your server defines the structure of 'orderData', which may differ
+                            var errorDetail = Array.isArray(orderData.details) && orderData.details[0];
+
+                            if (errorDetail && errorDetail.issue === 'INSTRUMENT_DECLINED') {
+                                // Recoverable state, see: "Handle Funding Failures"
+                                // https://developer.paypal.com/docs/checkout/integration-features/funding-failure/
                                 return actions.restart();
                             }
 
+                            if (errorDetail) {
+                                var msg = 'Sorry, your transaction could not be processed.';
+                                if (errorDetail.description) msg += '\\n\\n' + errorDetail.description;
+                                if (orderData.debug_id) msg += ' (' + orderData.debug_id + ')';
+                                // Show a failure message
+                                return alert(msg);
+                            }
+
                             // Show a success message to the buyer
-                            alert('Transaction completed by ' + details.payer.name.given_name + '!');
+                            alert('Transaction completed by ' + orderData.payer.name.given_name);
                         });
                     }
 

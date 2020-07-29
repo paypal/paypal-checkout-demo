0c54a51dfa364feab421a941cd983ef96570978c
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2020-07-29 17:05:24 -0500

    Use 'orderData' variable name

--- src/app/client/js/patterns/server.jsx
+++ src/app/client/js/patterns/server.jsx
@@ -41,8 +41,8 @@ export let server = {
                             method: 'post'
                         }).then(function(res) {
                             return res.json();
-                        }).then(function(serverData) {
-                            return serverData.id;
+                        }).then(function(orderData) {
+                            return orderData.id;
                         });
                     },
 
@@ -52,15 +52,15 @@ export let server = {
                             method: 'post'
                         }).then(function(res) {
                             return res.json();
-                        }).then(function(serverData) {
+                        }).then(function(orderData) {
                             // Three cases to handle:
                             //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
                             //   (2) Other non-recoverable errors -> Show a failure message
                             //   (3) Successful transaction -> Show confirmation or thank you
 
                             // This example reads a v2/checkout/orders capture response, propagated from the server
-                            // You could use a different API or structure for your 'serverData'
-                            var errorDetail = Array.isArray(serverData.details) && serverData.details[0];
+                            // You could use a different API or structure for your 'orderData'
+                            var errorDetail = Array.isArray(orderData.details) && orderData.details[0];
 
                             if (errorDetail && errorDetail.issue === 'INSTRUMENT_DECLINED') {
                                 return actions.restart(); // Recoverable state, per:
@@ -70,12 +70,12 @@ export let server = {
                             if (errorDetail) {
                                 var msg = 'Sorry, your transaction could not be processed.';
                                 if (errorDetail.description) msg += '\\n\\n' + errorDetail.description;
-                                if (serverData.debug_id) msg += ' (' + serverData.debug_id + ')';
+                                if (orderData.debug_id) msg += ' (' + orderData.debug_id + ')';
                                 return alert(msg); // Show a failure message
                             }
 
                             // Show a success message
-                            alert('Transaction completed by ' + serverData.payer.name.given_name);
+                            alert('Transaction completed by ' + orderData.payer.name.given_name);
                         });
                     }
 

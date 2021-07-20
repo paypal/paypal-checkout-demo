5a905c2e0215a17d3843853c2ab65ef2dfa4a642
Leonardo Gedler <lgedler@paypal.com>
2021-07-20 14:48:49 -0400

    Updated cliend side param name, changed it from details to orderData. Updated serve side success message

--- src/app/client/js/patterns/server.jsx
+++ src/app/client/js/patterns/server.jsx
@@ -75,9 +75,9 @@ export let server = {
                             }
 
                             // Successful capture! For demo purposes
-                            console.log('Capture result', details, JSON.stringify(details, null, 2));
-                            var transaction = details.purchase_units[0].payments.captures[0];
-                            // alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+                            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                            var transaction = orderData.purchase_units[0].payments.captures[0];
+                            alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
         
                             // Replace the above to show a success message within this page, e.g.
                             // const element = document.getElementById('paypal-button-container');
